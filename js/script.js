9e132c637b93a5529e86728295a3e43ebb30de82
Ahsana Azmiara Ahmadiham <ahsanaazmiara29@gmail.com>
2024-07-04 16:05:05 +0000

    first push

---
+++ js/script.js
@@ -0,0 +1,34 @@
+function validateForm() {
+  let name = document.querySelector('#nama').value;
+  let dob = document.querySelector('#tanggal').value; // dob stands for date of birth
+
+  // Get current time
+  let currentDate = new Date();
+  let time = currentDate.toLocaleTimeString(); // Format time for user's locale
+
+  // Get gender (assuming only two options are selected)
+  let gender = "";
+  if (document.getElementById('male').checked) {
+    gender = "Laki-Laki";
+  } else if (document.getElementById('female').checked) {
+    gender = "Perempuan";
+  }
+
+  // Get message
+  let message = document.querySelector('textarea[name="messages"]').value;
+
+  // Validate if all fields are filled
+  if (name != '' && dob != '' && message != '') {
+    // Build the output string
+    let output = `<b>Current Time:</b> ${time}<br>`;
+    output += `<b>Nama:</b> ${name}<br>`;
+    output += `<b>Tanggal Lahir:</b> ${dob}<br>`;
+    output += `<b>Jenis Kelamin:</b> ${gender}<br>`;
+    output += `<b>Pesan:</b> ${message}`;
+
+    // Display the output in the result div
+    document.querySelector('#result').innerHTML = output;
+  } else {
+    alert('Inputan ada yang kosong');
+  }
+}
